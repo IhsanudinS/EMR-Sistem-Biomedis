83b3904ba7a1930b6e330a148673d7c67273aa66
IhsanudinS <muhammadihsanudinsyaifullah@std.unissula.ac.id>
2023-11-06 21:01:30 +0700

    first commit

---
+++ konfig.php
@@ -0,0 +1,17 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| Aksi login
+|--------------------------------------------------------------------------
+|
+|   Aplikasi Sistem Informasi Rumah Sakit Sederhana
+|   http://www.sarjanakomedi.com
+|
+*/
+// error_reporting(0);
+
+        
+    $base_url = "http://localhost:2000/sik/";
+	
+	$con = mysqli_connect('localhost', 'root', '', 'rs');
+?>
